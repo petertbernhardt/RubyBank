d743e8e162ddd12c24a4a149d4e7896f1dd034ea
unknown <n0254847@VDDR03P-A5B87E0.lm.lmig.com>
2014-07-17 10:59:23 -0700

    Added a method for withdrawing money.

--- Account.rb
+++ Account.rb
@@ -9,8 +9,17 @@ class Account
   
   public
   def display_balance(pin_number)
-    if pin_number == 1234
+    if pin_number == pin
       puts "Balance: $#{balance}."
+    else
+      puts pin_error
+    end
+  end
+  
+  def withdraw(pin_number, amount)
+    if pin_number == pin
+      @balance -= amount
+      puts "Withdrew #{amount}. New balance: $#{@balance}."
     else
       puts pin_error
     end
