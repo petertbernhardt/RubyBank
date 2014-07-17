b459d65027ee1b8d7a1496e80c5c7673e5a775e9
unknown <n0254847@VDDR03P-A5B87E0.lm.lmig.com>
2014-07-17 11:07:14 -0700

    Added a method for depositing money, reduced redundancy in display_balance, withdraw, and deposit, and added check_pin to check if the given pin number is correct.

--- Account.rb
+++ Account.rb
@@ -9,19 +9,22 @@ class Account
   
   public
   def display_balance(pin_number)
-    if pin_number == pin
+    if check_pin(pin_number)
       puts "Balance: $#{balance}."
-    else
-      puts pin_error
     end
   end
   
   def withdraw(pin_number, amount)
-    if pin_number == pin
+    if check_pin(pin_number)
       @balance -= amount
       puts "Withdrew #{amount}. New balance: $#{@balance}."
-    else
-      puts pin_error
+    end
+  end
+  
+  def deposit(pin_number, amount)
+    if check_pin(pin_number)
+      @balance += amount
+      puts "Deposited #{amount}. New balance: $#{@balance}."
     end
   end
   
@@ -32,6 +35,15 @@ class Account
   
   def pin_error
     return "Access denied: incorrect PIN."
+  end
+  
+  def check_pin(pin_number)
+    if pin_number == pin
+      true
+    else
+      puts pin_error
+      false
+    end    
   end
 end
 
