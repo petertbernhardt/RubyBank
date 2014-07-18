c585cfe81b400348a8b062ae1afb6632452c4673
unknown <n0254847@VDDR03P-A5B87E0.lm.lmig.com>
2014-07-18 09:33:11 -0700

    Main.rb now attempts to find the user that tried to log in.

--- Main.rb
+++ Main.rb
@@ -1,6 +1,13 @@
+require 'DatabaseHandler'
+
 class Main
   puts "Please enter your name. "
   name = gets.chomp
   puts "Please enter your PIN. "
   pin = gets.chomp
+  handler = DatabaseHandler.new("bank.db")
+  query = "SELECT name, pin FROM users 
+            WHERE name = '#{name}' AND pin = #{pin}"
+  data = handler.execute(query)
+  puts data
 end
