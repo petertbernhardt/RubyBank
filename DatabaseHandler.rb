dcca5db491f4a63fc4acc67970a3e770a4d1e8ee
unknown <n0254847@VDDR03P-A5B87E0.lm.lmig.com>
2014-07-18 08:51:48 -0700

    DatabaseHandler now has an initialize method and a public execute method to run queries. Added some test code, which works. Added two entries to bank.db's users table.

--- DatabaseHandler.rb
+++ DatabaseHandler.rb
@@ -1,4 +1,19 @@
 require 'sqlite3'
 
 class DatabaseHandler
+  
+  def initialize(filename)
+    @database = SQLite3::Database.open(filename)
+  end
+  
+  public
+  def execute(query)
+    @database.execute(query)
+  end
 end
+
+handler = DatabaseHandler.new("bank.db")
+handler.execute("INSERT INTO users (name, pin) 
+                 VALUES ('Peter', 1992)")
+rows = handler.execute("SELECT * FROM users")
+puts rows
